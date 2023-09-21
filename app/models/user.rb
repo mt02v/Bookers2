14811043b11b1290d3572334b40aed52461529fd
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-21 09:16:38 +0000

    [Add] 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
          
-  has_many :Book_images, dependent: :destroy
+  has_many :book_images, dependent: :destroy
   
          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
-  get '/home/about' => "homees#about"
   devise_for :users
+  root to: "homes#top"
+  get '/about' => "homees#about"
+  post 'users' => "users#create"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :post_images, only: []
+resources :post_images, only: [:new]
 
 end
