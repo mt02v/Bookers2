980c9873ab92b49251d2dc387c57af193339b287
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-21 12:46:43 +0000

    [Add] aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get '/about' => "homees#about"
+  get 'homes/about' => "homes#about"
   post 'users' => "users#create"
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :post_images, only: [:new]
 
