b95c94bb25ff8fc64a3376011cae27179e1c072e
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-22 08:58:23 +0000

    ユーザのプロフィール更新が成功したとき

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,8 +36,33 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user =User.find(params[:id])
+    if  @user.update(user_params)
     flash[:notice] = "You have updated user successfully."
+    
     redirect_to "/users/#{current_user.id}"
-  else
+    
+    else
     flash[:notice] = "errors prohibited this obj from being saved:"
+    render :edit
+    end
+  end
+
+
+    private
+    
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+    
+    def user_params
+      params.require(:user).permit(:name,:profile_image,:introduction)
+    end
+    
+    def ensure_current_user
+      @user = User.find(params[:id])
+      if @user.id != current_user.id
+        redirect_to user_path(current_user.id)
+      end
+    end
 end
