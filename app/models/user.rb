8253c2b27060a2064f257a57e34ef8a235d1bc17
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-23 02:58:52 +0000

    [add] 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+                                    :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image 
   has_many :books, dependent: :destroy
-
-  validates :name, presence: true,length: {in: 2..20}
-  validates :introduction,length: { maximum: 50}
-
-  attachment :profile_image
-
-  def email_requires?
-    false
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
   end
-  def email_changed?
-    false
-  end
+
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50}
+  validates :name, uniqueness: true
+
 end
--- db/migrate/20230920072458_create_book_images.rb
+++
@@ -1,8 +0,0 @@
-class CreateBookImages < ActiveRecord::Migration[6.1]
-  def change
-    create_table :book_images do |t|
-
-      t.timestamps
-    end
-  end
-end
